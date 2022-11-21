49978f455362c332ce9342ecc010a8dfc42b4872
Veronica-Alfr <veagalves@gmail.com>
2022-11-21 17:58:19 -0300

    test: test_encrypt_message created

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    first_message = encrypt_message("AABBCC", -1)
+    assert first_message == "CCBBAA"
+
+    second_message = encrypt_message("AABBCC", 4)
+    assert second_message == "AC_CBBA"
+
+    third_message = encrypt_message("AABBCC", 3)
+    assert third_message == "BAA_CCB"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "key")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message([5], 2)
