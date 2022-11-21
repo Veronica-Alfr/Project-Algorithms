f4d20a7cc1a64ab9e40e1e34d67ce01df5818565
Veronica-Alfr <veagalves@gmail.com>
2022-11-21 17:32:15 -0300

    feat: function study_schedule created

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    total_students = 0
+
+    for entrance, exit in permanence_period:
+        if not isinstance(entrance, int) or not isinstance(exit, int):
+            return None
+        elif entrance <= target_time <= exit:
+            total_students += 1
+
+    return total_students
