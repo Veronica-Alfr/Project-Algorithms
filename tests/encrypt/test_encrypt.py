8c666ec389ea1a04fe43a47a75bd72258efdc0fb
Veronica-Alfr <veagalves@gmail.com>
2022-11-21 22:46:42 -0300

    fix: req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,16 +3,11 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    first_message = encrypt_message("AABBCC", -1)
-    assert first_message == "CCBBAA"
-
-    second_message = encrypt_message("AABBCC", 4)
-    assert second_message == "AC_CBBA"
-
-    third_message = encrypt_message("AABBCC", 3)
-    assert third_message == "BAA_CCB"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+    assert encrypt_message("AABBCC", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("message", "key")
     with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message([5], 2)
+        encrypt_message(5, 2)
